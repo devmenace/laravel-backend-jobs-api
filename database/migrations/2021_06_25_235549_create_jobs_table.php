4aea0331ee939644d569b6213c7356ec3664e91e
den <menacedev@gmail.com>
2021-06-26 01:15:48 +0100

    Added Basic Models and Migration files

---
+++ database/migrations/2021_06_25_235549_create_jobs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('summary', 150);
+            $table->string('description', 500);
+            $table->enum('status', ['open', 'in_progress', 'completed', 'cancelled']);
+            $table->integer('property_relation',);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
